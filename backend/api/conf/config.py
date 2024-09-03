b1d920fcfe1b443575711fb79226ea83bd00a3d2
Hoopan <hongpanzhou@gmail.com>
2024-09-03 07:48:21 +0000

    Claude model

--- backend/api/conf/config.py
+++ backend/api/conf/config.py
@@ -115,7 +115,7 @@ class OpenaiApiSetting(BaseModel):
     proxy: Optional[str] = None
     connect_timeout: int = Field(10, ge=1)
     read_timeout: int = Field(20, ge=1)
-    enabled_models: list[OpenaiApiChatModels] = ["gpt_3_5", "gpt_4", "gpt_4o"]
+    enabled_models: list[OpenaiApiChatModels] = ["gpt_3_5", "gpt_4", "gpt_4o", "claude_3_haiku"]
     model_code_mapping: dict[OpenaiApiChatModels, str] = {
         "gpt_3_5": "gpt-4o-mini",
         "gpt_4": "gpt-4",
